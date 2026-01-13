9f328e461be451df3f189824facb7f47a20b29e9
Toolbox <danielalejandro.leivafunes9@gmail.com>
2026-01-12 21:22:16 -0600

    Added shield ability

--- player.gd
+++ player.gd
@@ -2,11 +2,17 @@ extends CharacterBody2D
 
 @export var speed := 400
 @export var mode := "bass"
+@export var bass_color := Color.from_rgba8(0, 0, 180, 255)
+@export var treble_color := Color.from_rgba8(180, 0, 0, 255)
+
+@onready var shield_timer = $ShieldTimer
+
+var is_shielded := false
 
 signal mode_switched
 
 func _ready() -> void:
-	modulate = Color.from_rgba8(0, 0, 180, 255) # Default color for bass mode
+	modulate = bass_color # Default color for bass mode
 
 func get_input() -> void:
 	var input_direction := Input.get_vector("left", "right", "up", "down")
@@ -15,18 +21,37 @@ func get_input() -> void:
 func switch_mode() -> void:
 	if mode == "bass":
 		mode = "treble"
-		modulate = Color.from_rgba8(180, 0, 0, 255)
+		modulate_color(mode)
 		mode_switched.emit() # This signal is emitted on mode switch
 		return
 	if mode == "treble":
 		mode = "bass"
-		modulate = Color.from_rgba8(0, 0, 180, 255)
+		modulate_color(mode)
 		mode_switched.emit() # This signal is emitted on mode switch... again
 		return
 
+func modulate_color(current_mode) -> void:
+	if current_mode == "bass":
+		modulate = bass_color
+		return
+	if current_mode == "treble":
+		modulate = treble_color
+		return
+
+func shield() -> void:
+	modulate = Color.AQUA
+	is_shielded = true
+	shield_timer.start()
 
 func _physics_process(_delta: float) -> void:
 	get_input()
 	move_and_slide()
 	if Input.is_action_just_pressed("switch_mode"):
 		switch_mode()
+	
+	if mode == "bass" and Input.is_action_just_pressed("click") and is_shielded == false:
+		shield()
+
+func _on_shield_timer_timeout() -> void: # Callback for shield timer
+	modulate_color(mode)
+	is_shielded = false
