c46e42d40bcecf5690e58b181c41746721cc6d29
typhzilla <36720319+typhzilla@users.noreply.github.com>
2026-01-12 22:36:36 -0500

    added basic screen transitions

---
+++ menu/screen_trans.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+func load_in(path : String): ##must be a string that contains a resource path 
+	var scene = load(path).instantiate()
+	
+	$AnimationPlayer.play("load_in")
+	await $AnimationPlayer.animation_finished
+	
+	get_tree().change_scene_to_file(path)
+	
+	load_out()
+	
+func load_out():
+	$AnimationPlayer.play("load_out")
+	await $AnimationPlayer.animation_finished
