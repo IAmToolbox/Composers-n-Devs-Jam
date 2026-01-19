08d6db28e1fc4c352d64e064ed698169d968d766
Toolbox <danielalejandro.leivafunes9@gmail.com>
2026-01-18 23:20:23 -0600

    Implemented spritesheet for bass enemies. Needs some work still

--- components/bass_enemy.gd
+++ components/bass_enemy.gd
@@ -1,7 +1,10 @@
 class_name BassEnemy extends Enemy
 
+@onready var sprite = $AnimatedSprite2D
+
 func _ready() -> void:
 	speed = randf_range(1,3)
+	sprite.play()
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
@@ -12,7 +15,7 @@ func _move_towards_player() -> void:
 		position.x = move_toward(position.x, get_node("/root/mainLevel/player").position.x, acceleration)
 		position.y = move_toward(position.y, get_node("/root/mainLevel/player").position.y, acceleration)
 
-func _process(delta) -> void:
+func _process(_delta) -> void:
 	
 	_move_towards_player()
 	move_and_slide()
