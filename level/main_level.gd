2497269b6b176cd0d79aa771794643d5aa3bf0d0
Toolbox <danielalejandro.leivafunes9@gmail.com>
2026-01-21 18:00:00 -0600

    Added music from Karl, as well as the ability to switch between both music tracks

--- level/main_level.gd
+++ level/main_level.gd
@@ -4,10 +4,25 @@ extends Node2D
 
 @onready var player = $player
 @onready var label = $Label
+@onready var bass_music = $BassMusic
+@onready var treble_music = $TrebleMusic
 
+func _ready() -> void:
+	var treble_bus_index = AudioServer.get_bus_index("TrebleMusic")
+	AudioServer.set_bus_mute(treble_bus_index, true)
 
 func _on_player_mode_switched() -> void:
 	label.text = "Mode: " + player.mode
+	
+	# This spaghetti switches between the two music tracks playing
+	var bass_bus_index = AudioServer.get_bus_index("BassMusic")
+	var treble_bus_index = AudioServer.get_bus_index("TrebleMusic")
+	if AudioServer.is_bus_mute(bass_bus_index):
+		AudioServer.set_bus_mute(treble_bus_index, true)
+		AudioServer.set_bus_mute(bass_bus_index, false)
+	elif AudioServer.is_bus_mute(treble_bus_index):
+		AudioServer.set_bus_mute(treble_bus_index, false)
+		AudioServer.set_bus_mute(bass_bus_index, true)
 
 func _on_enemy_spawn_timer_timeout() -> void: # TODO: Replace this function with something that allows the enemy to actually follow the player
 	var enemy = enemy_scene[randi_range(0,1)].instantiate()
